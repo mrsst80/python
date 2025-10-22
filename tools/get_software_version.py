8abdc53ac81e6599937de13e4eeffe511cd5d894
Stefan Stefanov <mr.sst@icloud.com>
2025-10-23 00:01:30 +0300

    running commands in parallel in get_software_version.py

--- tools/get_software_version.py
+++ tools/get_software_version.py
@@ -4,14 +4,17 @@ import subprocess
 import shlex
 import re
 
-#ssh_connect_string = "ssh -T -oStrictHostKeyChecking=no -oConnectTimeout=10 -oBatchMode=yes -i {} {}@{}".format(ssh_key,hmc_username,hmc_hostname)
+ssh_key = "id_rsa_hmc"
+ssh_user = "stefan"
+software_name = "nxlog"
 
 def get_servers_list():
    mac_groups = []
-   servers = []
+   servers = {}
    cmd_get_mac_groups = "lsnim -t mac_group"
    cmd_get_mac_groups = shlex.split(cmd_get_mac_groups)
    mac_group_pattern = r"(.*Group)" 
+   #mac_group_pattern = r"(Power8-S824-Ire-Group)" 
    server_pattern = r"(member.*)= (.*)"
   
    mac_groups_tmp = subprocess.run(cmd_get_mac_groups, text=True,capture_output=True) 
@@ -28,10 +31,52 @@ def get_servers_list():
      for line in servers_list.stdout.split("\n"):
        match = re.search(server_pattern, line)
        if match:
-         servers.append(match.group(2))
+         servers[match.group(2)] = "1"
 
    return servers
 
-print(get_servers_list())
-  
+def get_software_version(servers,software_name):
+   for server in servers.keys():
+     cmd_get_software_version = "rpm -q {}".format(software_name)
+     ssh_connect_string = "ssh -T -oStrictHostKeyChecking=no -oConnectTimeout=10 -oBatchMode=yes -i {} {}@{}".format(ssh_key,ssh_user,server)
+     ssh_connect_string = shlex.split(ssh_connect_string)
 
+     proceses = [subprocess.Popen(ssh_connect_string) ]
+
+     with subprocess.Popen(ssh_connect_string,
+                        stdin=subprocess.PIPE, stdout=subprocess.PIPE,text=True) as ssh:
+
+       out, err = ssh.communicate(cmd_get_software_version)
+       print(out)
+
+def run_ssh_parallel(servers,software_name):
+   cmd_get_software_version = "rpm -q {}".format(software_name)
+   #ssh_connect_string = "ssh -T -oStrictHostKeyChecking=no -oConnectTimeout=10 -oBatchMode=yes -i {} {}@{}".format(ssh_key,ssh_user,server)
+
+   cmd_list = []
+   
+   for server in servers:
+     cmd = "ssh -T -q -oStrictHostKeyChecking=no -oConnectTimeout=10 -oBatchMode=yes -i {} -l {} {} \"rpm -q {}\"".format(ssh_key,ssh_user,server,software_name)
+     cmd_list.append(cmd)
+     #print(cmd)
+
+   procs_list = [subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0) for cmd in cmd_list]
+   for proc in procs_list:
+     proc.wait()
+     print(proc.args[10])
+     if proc.returncode == 0:
+        print(proc.stdout.readlines())
+     else:
+        print(proc.stderr.readlines())
+     #out, err = proc.communicate()
+     #print(proc.stdout.readlines())
+     #print(proc.stderr.readlines())
+     #print(proc.returncode)
+ 
+def main():
+  servers = get_servers_list()
+  
+  #get_software_version(servers,software_name)
+  run_ssh_parallel(servers,software_name)
+  
+main()
